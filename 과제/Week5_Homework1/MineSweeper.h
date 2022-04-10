dcc0b99d1708d0aeba6cd2e1eb4a8c521e06f6fa
dlgusals97 <59655095+Lee-Hyeon-Min@users.noreply.github.com>
2022-04-10 21:25:20 +0900

    Add files via upload

---
+++ 과제/Week5_Homework1/MineSweeper.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
+#include <conio.h>
+#include <ctype.h>
+#include <memory.h>
+extern void playMineSweeper(int nBomb = 13);
---
+++ 과제/Week5_Homework1/main.cpp
@@ -0,0 +1,8 @@
+#include "MineSweeper.h"
+int main()
+{
+	int total;
+	printf("Áö·Ú ÃÑ °³¼ö ÀÔ·Â:");
+	scanf_s("%d", &total);
+	playMineSweeper(total);
+}
