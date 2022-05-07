cd0fd3f04bca28a86e47b6ff47ae4c534b290317
dlgusals97 <59655095+Lee-Hyeon-Min@users.noreply.github.com>
2022-05-07 22:24:34 +0900

    Update MonsterWorld.h

--- 과제/Week7_Homework1/MonsterWorld.h
+++ 과제/Week7_Homework1/MonsterWorld.h
@@ -23,7 +23,7 @@ class MonsterWorld {
 		canvas.clear();
 		for (int y = 0; y < yMax; y++)
 			for (int x = 0; x < xMax; x++)
-				if (Map(y, x) > 0) canvas.draw(x, y, "■");
+				if (Map(x, y) > 0) canvas.draw(x, y, "■");
 		for (int i = 0; i < nMon; i++)
 			mon[i].draw(canvas);
 		canvas.print("[Monster World : Basic]");
